dbccc4c124961dfb5f6ee6f22e144af0466c4d22
Ethan Chen <ethansunchen@gmail.com>
2023-12-29 00:22:54 -0700

    xd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 // ...
 import Greet from './greet'
+import Task from './task'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Greet />
-      ...
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-orange-400">
+      <Task></Task>
     </main>
   )
 }
