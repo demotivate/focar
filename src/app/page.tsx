617aff5195000dd1315d46f0a0059fa912122044
Ethan Chen <ethansunchen@gmail.com>
2024-01-03 00:25:35 -0700

    complete base of navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,22 +9,35 @@ import { MiniButton } from './button'
 
 export default function Home() {
   return (
-    <main className="flex flex-row min-h-screen justify-between bg-dPrimary font-sans text-dText">
-
-      <div id='Sidebar' className='flex flex-col basis-1/6 bg-dSecondary'>
-        <Sidebar.SidebarTab section="All Tasks"></Sidebar.SidebarTab>
-        <Sidebar.SidebarTab section="My Day"></Sidebar.SidebarTab>
-        <Sidebar.SidebarTab section="My Week"></Sidebar.SidebarTab>
-      </div>
-
-      <div id='Body' className='flex flex-col basis-5/6'>
-        <div id='View Switch Menu' className='flex flex-row-reverse basis-1 grow-0'>
-          <MiniButton image="calendar.svg" alt='Calendar View'></MiniButton>
-          <MiniButton image="check_circle.svg" alt='Calendar View'></MiniButton>
+    <div data-theme="luxury">
+      <div className="navbar bg-base-100">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
+            </div>
+            <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+              <li><a>Homepage</a></li>
+              <li><a>Portfolio</a></li>
+              <li><a>About</a></li>
+            </ul>
+          </div>
         </div>
-        <div id='Todos' className='flex flex-col basis-3/4'></div>
-        <div id='Todo Input' className='basis-1/6'></div>
+        <div className="navbar-center">
+          <a className="btn btn-ghost text-xl">focar</a>
+        </div>
+        <div className="navbar-end">
+          <button className="btn btn-ghost btn-circle">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 -960 960 960" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="80" d="M200-80q-33 0-56.5-23.5T120-160v-560q0-33 23.5-56.5T200-800h40v-80h80v80h320v-80h80v80h40q33 0 56.5 23.5T840-720v560q0 33-23.5 56.5T760-80H200Zm0-80h560v-400H200v400Zm0-480h560v-80H200v80Zm0 0v-80 80Zm280 240q-17 0-28.5-11.5T440-440q0-17 11.5-28.5T480-480q17 0 28.5 11.5T520-440q0 17-11.5 28.5T480-400Zm-160 0q-17 0-28.5-11.5T280-440q0-17 11.5-28.5T320-480q17 0 28.5 11.5T360-440q0 17-11.5 28.5T320-400Zm320 0q-17 0-28.5-11.5T600-440q0-17 11.5-28.5T640-480q17 0 28.5 11.5T680-440q0 17-11.5 28.5T640-400ZM480-240q-17 0-28.5-11.5T440-280q0-17 11.5-28.5T480-320q17 0 28.5 11.5T520-280q0 17-11.5 28.5T480-240Zm-160 0q-17 0-28.5-11.5T280-280q0-17 11.5-28.5T320-320q17 0 28.5 11.5T360-280q0 17-11.5 28.5T320-240Zm320 0q-17 0-28.5-11.5T600-280q0-17 11.5-28.5T640-320q17 0 28.5 11.5T680-280q0 17-11.5 28.5T640-240Z" /></svg>
+          </button>
+          <button className="btn btn-ghost btn-circle">
+            <div className="indicator">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 -960 960 960" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="80" d="m424-296 282-282-56-56-226 226-114-114-56 56 170 170Zm56 216q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z" /></svg>
+              <span className="badge badge-xs badge-primary indicator-item"></span>
+            </div>
+          </button>
+        </div>
       </div>
-    </main>
+    </div>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,10 @@ const config: Config = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["dark"],
+  },
   theme: {
     extend: {
       backgroundImage: {
@@ -13,26 +17,20 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
-      colors: {
-        'dPrimary': '#242424',
-        'dSecondary': '#171717',
-        'dText': '#c4c4c4',
-        'dBlack': '#092635',
-        'dTeal': '#1B4242',
-        'dSage': '#5C8374',
-        'dSageLight': '#9EC8B9',
-      },
+      // colors: {
+      //   'dPrimary': '#242424',
+      //   'dSecondary': '#171717',
+      //   'dText': '#c4c4c4',
+      //   'dBlack': '#092635',
+      //   'dTeal': '#1B4242',
+      //   'dSage': '#5C8374',
+      //   'dSageLight': '#9EC8B9',
+      // },
     },
     fontFamily: {
       sans: ['Kanit', 'sans-serif']
     }
   },
-  plugins: [],
 }
-
-// module.exports = {
-//   //...
-//   plugins: [require("daisyui")],
-// }
 
 export default config
